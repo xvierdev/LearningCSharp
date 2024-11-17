d12d7bab6b504b1be385daf1ccd6bd4ead269736
Wesley Xavier <wesley.xvier@gmail.com>
2024-11-17 17:08:06 -0300

    :wrench: chore: modified gitignore file.

---
+++ Udemy/CSharp/Excercices/Ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Globalization;
+namespace Exercices{
+    class Program{
+        static void Main (string[] args){
+            
+        }
+    }
+}
---
+++ Udemy/CSharp/Excercices/model/Program.cs
@@ -0,0 +1,8 @@
+using System;
+namespace Exercices{
+    class Program{
+        static void Main (string[] args){
+            // Put your code here.
+        }
+    }
+}
