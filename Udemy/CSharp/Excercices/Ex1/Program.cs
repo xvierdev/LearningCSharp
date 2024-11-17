7daa75be4a95b4a3cca4c7d091f92890b113318c
Wesley Xavier <wesley.xvier@gmail.com>
2024-11-17 16:48:13 -0300

    :books: chore: added docs and two first exercices.

---
+++ Udemy/CSharp/Excercices/Ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Exercices{
+    class Program{
+        static void Main (string[] args){
+            int a, b, soma;
+            Console.WriteLine("Enter first number: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            b = int.Parse(Console.ReadLine());
+            soma = a + b;
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
---
+++ Udemy/CSharp/Excercices/Ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+namespace Exercices{
+    class Program{
+        static void Main (string[] args){
+            const double PI = 3.14159;
+            double area, raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = PI * Math.Pow(raio, 2);
+            Console.WriteLine("AREA={0}", area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
