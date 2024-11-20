e2a34f76f1f8e94a938e6036e3babf3596388a4b
Weley Xavier <wesley.xvier@gmail.com>
2024-11-19 21:06:13 -0300

    :books: chore: added more exercicies.

--- Udemy/CSharp/Excercices/List4/Ex4/Program.cs
+++ Udemy/CSharp/Excercices/List4/Ex4/Program.cs
@@ -12,7 +12,12 @@ namespace Exercices
                 string[] input = Console.ReadLine().Split(' ');
                 a = double.Parse(input[0], CultureInfo.InvariantCulture);
                 b = double.Parse(input[1], CultureInfo.InvariantCulture);
-                Console.WriteLine("{0}", a.ToString("F2", CultureInfo.InvariantCulture));
+                if(b != 0){
+                    Console.WriteLine("{0}", a.ToString("F1", CultureInfo.InvariantCulture));
+                }
+                else{
+                    Console.WriteLine("divisao impossivel");
+                }
             }
         }
     }
