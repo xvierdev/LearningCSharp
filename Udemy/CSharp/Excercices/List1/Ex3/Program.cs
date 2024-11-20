5809664520dcfa7799ee5b3130957f5d1213dff0
Wesley Xavier <wesley.xvier@gmail.com>
2024-11-20 07:17:47 -0300

    :books: docs: remove unused libraries

--- Udemy/CSharp/Excercices/List1/Ex3/Program.cs
+++ Udemy/CSharp/Excercices/List1/Ex3/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 namespace Exercices
 {
     class Program
--- Udemy/CSharp/Excercices/List2/Ex3/Program.cs
+++ Udemy/CSharp/Excercices/List2/Ex3/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Globalization;
 namespace Exercices
 {
     class Program
