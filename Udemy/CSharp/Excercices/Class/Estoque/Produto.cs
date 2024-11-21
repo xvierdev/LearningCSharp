1695d4e7935e935297fb66f48261fd60c2e5ae17
Wesley Xavier <wesley.xvier@gmail.com>
2024-11-21 14:25:11 -0300

    :books: docs: update class Produto.

--- Udemy/CSharp/Excercices/Class/Estoque/Produto.cs
+++ Udemy/CSharp/Excercices/Class/Estoque/Produto.cs
@@ -4,6 +4,9 @@ namespace Exercices{
         public double Preco;
         public int Quantidade;
 
+        public override string ToString(){
+            return $"{Nome}, $ {Preco:C2}, {Quantidade} unidades, Total: $ {Quantidade * Preco:C2}";
+        }
         public double ValorTotalEmEstoque(){
             return Preco * Quantidade;
         }
--- Udemy/CSharp/Excercices/Class/Estoque/Program.cs
+++ Udemy/CSharp/Excercices/Class/Estoque/Program.cs
@@ -15,22 +15,19 @@ namespace Exercices
             Console.Write("Quantidade do produto: ");
             produto.Quantidade = int.Parse(Console.ReadLine());
 
-            PrintEstoque(produto);
+            Console.WriteLine("Dados do produto: " + produto.ToString());
 
             Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
             produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
 
-            PrintEstoque(produto);
+            Console.WriteLine("Dados do produto: " + produto.ToString());
 
             Console.Write("Digite o número de produtos a ser removido do estoque: ");
             produto.RemoverProdutos(int.Parse(Console.ReadLine()));
 
-            PrintEstoque(produto);
+            Console.WriteLine("Dados do produto: " + produto.ToString());
 
         }
 
-        static void PrintEstoque (Produto prod){
-            Console.WriteLine("Dados do produto: {0}, $ {1}, {2} unidades, Total: $ {3}", prod.Nome, prod.Preco.ToString("F2", CultureInfo.InvariantCulture), prod.Quantidade, prod.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture));
-        }
     }
 }
