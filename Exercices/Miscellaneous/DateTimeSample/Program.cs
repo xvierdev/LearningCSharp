db176e4553f5b7dda107e5a79939f681c573461d
Wesley Xavier <wesley.xvier@gmail.com>
2025-01-03 09:05:02 -0300

    :truck: chore: moved files to optimize organization.

---
+++ Exercices/Miscellaneous/DateTimeSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace DateTimeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime;
+            while (true)
+            {
+                dateTime = DateTime.Now;
+                Console.Write(dateTime.ToString("HH:mm:ss"));
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+    }
+}
