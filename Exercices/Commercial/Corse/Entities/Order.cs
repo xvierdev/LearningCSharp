2a662047560687a2af9849e1de5620573c6f5f0f
Weley Xavier <wesley.xvier@gmail.com>
2025-01-04 12:09:40 -0300

    :sparkles: :feat: added enum first example.
    added first code example with enumeration structures for commercial projects.

---
+++ Exercices/Commercial/Corse/Entities/Order.cs
@@ -0,0 +1,20 @@
+using System;
+using Corse.Entities.Enums;
+
+namespace Corse.Entities{
+    class Order{
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public override string ToString(){
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+
+    }
+}
---
+++ Exercices/Commercial/Corse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Corse.Entities;
+using Corse.Entities.Enums;
+
+namespace Corse{
+    class Program{
+        static void Main(string[] args){
+            Order order = new Order{
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Shipped");
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
